0c1550a0aa30e4af0ce13eca8aa46908ac68eea3
TheSkyentist <raphael.hviding@gmail.com>
2025-01-30 14:44:22 +0000

    Updated for v4

--- RUBIES/Targets/getTargets.py
+++ RUBIES/Targets/getTargets.py
@@ -4,34 +4,8 @@
 import argparse
 import requests
 import numpy as np
+from bs4 import BeautifulSoup
 from astropy.table import Table
-
-# Column Labels
-labels = (
-    'root',
-    'jname',
-    'uid',
-    'ndup',
-    'file',
-    'ra',
-    'dec',
-    'srcid',
-    'grating',
-    'filter',
-    'grade',
-    'zfit',
-    'z',
-    'comment',
-    'sn50',
-    'wmin',
-    'wmax',
-    'HST',
-    'NIRCam',
-    'slit',
-    'FITS',
-    'Fnu',
-    'Flam',
-)
 
 
 def main() -> None:
@@ -52,12 +26,12 @@ def main() -> None:
         '--url',
         type=str,
         help='DJA URL',
-        default='https://s3.amazonaws.com/msaexp-nirspec/extractions/rubies_all_extractions_v3.json',
+        default='https://s3.amazonaws.com/msaexp-nirspec/extractions/rubies_prelim_v4',
     )
     parser.add_argument(
         '--prepend-version',
         type=str,
-        default='nod-',
+        default='',
         help='String to prepend to version',
     )
     parser.add_argument(
@@ -94,46 +68,74 @@ def download(url: str, pv: str, pid: int) -> None:
     """
 
     # Get targets
-    r = requests.get(url)
+    r = requests.get(f'{url}.json')
     if r.status_code == 200:
         # Get the data
         data = r.json()['data']
-
-        # Create table
-        table = Table(np.array(data), names=labels, dtype=[type(d) for d in data[0]])
-
-        # Add program ID
-        table.add_column(pid, name='pid', index=1)
-
-        # Convert comment column to string
-        table['comment'] = table['comment'].astype(str)
-
-        # Fix comment column
-        bad_comment = np.logical_or(table['comment'] == 'None', table['comment'] == ';')
-        table['comment'][bad_comment] = ''
-
-        # Remove unneeded columns
-        table.remove_columns(['HST', 'NIRCam', 'slit', 'FITS', 'Fnu', 'Flam'])
-
-        # Add columns
-        table.add_columns(
-            np.array([r.split('-') for r in table['root']]).T,
-            names=['survey', 'field', 'version'],
-            indexes=[1, 1, 1],
-        )
-
-        # Add version info to file
-        version = table['version']
-        for key in ['root', 'file', 'version']:
-            table[key] = [
-                val.replace(ver, pv + ver) for val, ver in zip(table[key], version)
-            ]
-
-        # Save table
-        table.write('targets.fits', overwrite=True)
-
     else:
         raise ValueError('Failed to download targets from DJA')
+
+    # Get labels
+    r = requests.get(f'{url}.html')
+    if r.status_code == 200:
+        # Get the labels
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        # Find the only thead block
+        thead = soup.find('thead')
+
+        # Extract column headers into a list
+        labels = [th.text.strip() for th in thead.find_all('th')]
+    else:
+        raise ValueError('Failed to download labels from DJA')
+
+    # Get targets
+    r = requests.get(f'{url}.json')
+    if r.status_code == 200:
+        # Get the data
+        data = r.json()['data']
+    else:
+        raise ValueError('Failed to download targets from DJA')
+
+    # Create table
+    delim = '||'
+    table = Table(
+        np.genfromtxt(
+            [delim.join([str(i) for i in d]) for d in data],
+            delimiter=delim,
+            dtype=None,
+            names=labels,
+            missing_values='None',
+        )
+    )
+    table.rename_column('file_','file')
+
+    # Add program ID
+    table.add_column(pid, name='pid', index=1)
+
+    # Fix comment column
+    bad_comment = np.logical_or(table['comment'] == 'None', table['comment'] == ';')
+    table['comment'][bad_comment] = ''
+
+    # Get rid of all columns after comment
+    table.remove_columns(table.colnames[table.colnames.index('comment') + 1 :])
+
+    # Add columns
+    table.add_columns(
+        np.array([r.split('-') for r in table['root']]).T,
+        names=['survey', 'field', 'reduction'],
+        indexes=[1, 1, 1],
+    )
+
+    # Add version info to file
+    version = table['reduction']
+    for key in ['root', 'file', 'reduction']:
+        table.replace_column(
+            key, [val.replace(ver, pv + ver) for val, ver in zip(table[key], version)]
+        )
+
+    # Save table
+    table.write('targets.fits', overwrite=True)
 
 
 # Call main function
